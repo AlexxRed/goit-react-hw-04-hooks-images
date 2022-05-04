9be6f2de4a18b3217677846807ff450f0fe5667c
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-05-04 21:44:27 +0300

    need add axios params

--- src/api/getImages.js
+++ src/api/getImages.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '5132282-75e364beaf68381714aa1df4d';
-const per_page = 12;
+const PER_PAGE = 12;
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
 export function getImages(searchQuery, page) {
     const response = axios.get(
-`?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`
+`?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
     )
     return response;
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { Section } from "./App.styled";
 import { Loader } from "./Loader/Loader";
@@ -14,111 +14,70 @@ infoStyle()
 
 
 
+export function App() {
+  const [images, setImages] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [bigImage, setBigImage] = useState([]);
 
+  const onSubmit = searchQuery => {
+    setSearchQuery(searchQuery)
+    setImages([]);
+    setIsLoading(false);
+    setPage(1);
+  };
 
-
-
-
-export class App extends Component {
-  state = {
-    images: [],
-    searchQuery: '',
-    page: 1,
-    isLoading: false,
-    showModal: false,
-    bigImage: [],
-    totalHits: 0,
-    sumImages: 0,
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { page, searchQuery } = this.state;
-    if (prevState.page !== page || prevState.searchQuery !== searchQuery) {
-      try {
-        this.setState({ isLoading: true })
-        getImages(searchQuery, page).then(r => {
-
-          let customImagesData = mapper(r.data.hits)
-          // console.log(customImagesData);
-          // console.log(r.data);
-          if (r.data.hits.length === 0) {
-            this.setState({ isLoading: false });
-            Notify.info('we do not have this images');
-            // console.log('we do not have this images');
-            return;
-          }
-          this.setState(prevState => {
-            return {
-              images: [...prevState.images, ...customImagesData],
-              isLoading: false,
-              totalHits: r.data.totalHits,
-              sumImages: 0,
-            }
-          })
-        })
-        
-
-      } catch (error) {
-        this.setState({ isLoading: false })
-        console.log(error);
-      }
-      
+  useEffect(() => {
+    if (!searchQuery) {
+      return
     }
     
-  }
+    try {
+      setIsLoading(true);
+      
+      getImages(searchQuery, page).then(r => {
+        const customImagesData = mapper(r.data.hits);
 
-  onSubmit = searchQuery => {
-    this.setState({
-      images: [],
-      searchQuery: searchQuery,
-      page: 1,
-      isLoading: false,
-      showModal: false,
-      bigImage: [],
-      totalHits: 0,
-      sumImages: 0,
-    })
-  }
+        if (r.data.hits.length === 0) {
+          setIsLoading(false);
+          Notify.info('we do not have this images');
+          return;
+        };
+        
+        setImages([...images, ...customImagesData]);
+        setIsLoading(false);
+      })
+    } catch (error) {
+      setIsLoading(false);
+      Notify.failure(`${error}`);
+    };
+      
 
-  loadMore = () => {
-    // this.setState(prevState => {
-    //   return{
-    //   page: prevState.page += 1,
-    // }
-    // })
-    // console.log('load image');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery, page]);
+  
 
-    let { page } = this.state;
-    page += 1;
-    this.setState({ page });
-  }
+  const loadMore = () => {
+    setPage(page + 1);
+  };
 
-  toggleModal = () => {
-    this.setState(prevState => {
-      return {
-        showModal: !prevState.showModal,
-      }
-    })
-  }
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
+  };
 
-  showImage = (id) => {
-    const { toggleModal } = this;
-    const { images } = this.state;
-    images.map(image => {
+  const showImage = (id) => {
+    images.filter(image => {
       if (image.id === id) {
-        toggleModal()
-        return this.setState({bigImage: image})
+        toggleModal();
+        setBigImage(image);
       }
       return image
     })
+  };
 
-  }
-
-  render() {
-    const { images, isLoading, bigImage, showModal } = this.state; //totalHits, sumImages
-    const { onSubmit, loadMore, toggleModal, showImage } = this; 
-    // console.log(this.state.searchQuery);
-    return (
+  return (
       <Section>
         <Searchbar onSubmit={onSubmit} />
 
@@ -146,5 +105,137 @@ export class App extends Component {
         )}
       </Section>
     );
-  }
-};
+}
+
+
+
+
+// export class App2 extends Component {
+//   state = {
+//     images: [],
+//     searchQuery: '',
+//     page: 1,
+//     isLoading: false,
+//     showModal: false,
+//     bigImage: [],
+//     totalHits: 0,
+//     sumImages: 0,
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { page, searchQuery } = this.state;
+//     if (prevState.page !== page || prevState.searchQuery !== searchQuery) {
+//       try {
+//         this.setState({ isLoading: true })
+//         getImages(searchQuery, page).then(r => {
+
+//           let customImagesData = mapper(r.data.hits)
+//           // console.log(customImagesData);
+//           // console.log(r.data);
+//           if (r.data.hits.length === 0) {
+//             this.setState({ isLoading: false });
+//             Notify.info('we do not have this images');
+//             // console.log('we do not have this images');
+//             return;
+//           }
+//           this.setState(prevState => {
+//             return {
+//               images: [...prevState.images, ...customImagesData],
+//               isLoading: false,
+//               totalHits: r.data.totalHits,
+//               sumImages: 0,
+//             }
+//           })
+//         })
+        
+
+//       } catch (error) {
+//         this.setState({ isLoading: false })
+//         console.log(error);
+//       }
+      
+//     }
+    
+//   }
+
+//   onSubmit = searchQuery => {
+//     this.setState({
+//       images: [],
+//       searchQuery: searchQuery,
+//       page: 1,
+//       isLoading: false,
+//       showModal: false,
+//       bigImage: [],
+//       totalHits: 0,
+//       sumImages: 0,
+//     })
+//   }
+
+//   loadMore = () => {
+//     // this.setState(prevState => {
+//     //   return{
+//     //   page: prevState.page += 1,
+//     // }
+//     // })
+//     // console.log('load image');
+
+//     let { page } = this.state;
+//     page += 1;
+//     this.setState({ page });
+//   }
+
+//   toggleModal = () => {
+//     this.setState(prevState => {
+//       return {
+//         showModal: !prevState.showModal,
+//       }
+//     })
+//   }
+
+//   showImage = (id) => {
+//     const { toggleModal } = this;
+//     const { images } = this.state;
+//     images.map(image => {
+//       if (image.id === id) {
+//         toggleModal()
+//         return this.setState({bigImage: image})
+//       }
+//       return image
+//     })
+
+//   }
+
+//   render() {
+//     const { images, isLoading, bigImage, showModal } = this.state; //totalHits, sumImages
+//     const { onSubmit, loadMore, toggleModal, showImage } = this; 
+//     // console.log(this.state.searchQuery);
+//     return (
+//       <Section>
+//         <Searchbar onSubmit={onSubmit} />
+
+//         { isLoading &&
+//           !images.length ?
+//           (<Loader />) :
+//           (images.length > 0 &&
+//           (<ImageGallery images={images} showImage={showImage} />))
+//         }
+
+//         { isLoading &&
+//           images.length  &&
+//           <Loader />
+//         }
+
+//         {/* totalHits !== sumImages && */}
+//         { 
+//           images.length > 0 &&
+//           isLoading === false &&
+//           (<LoadMoreButton loadMore={loadMore} />)
+//         }
+
+//         {showModal && (
+//           <Modal toggleModal={toggleModal} bigImage={bigImage} />
+//         )}
+//       </Section>
+//     );
+//   }
+// };
